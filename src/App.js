b9d17da7d6981ef8d516d0f19e2a7ffef2434012
Devin Kenney <devin.kenney111@gmail.com>
2021-05-25 13:52:50 -0400

    added a couple of packages and made a baseline for the SPA in App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const handleInput = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Switch>
+        <Route
+          path="/new"
+          render={() => {
+            return(
+              <New
+                handleInput={handleInput}
+              />
+            )
+          }}
+        />
+        <Route
+          path="/view"
+          render={() => {
+            return(
+              <View/>
+            )
+          }}
+        />
+      </Switch>
     </div>
   );
 }

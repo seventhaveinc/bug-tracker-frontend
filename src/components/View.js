875ced0cfddfe48643d372f6ca6c30d3d863aec9
Devin Kenney <devin.kenney111@gmail.com>
2021-05-26 19:10:38 -0400

    Added a bunch of stuff including more styling on the view page and separation between bug reports and feature requests

--- src/components/View.js
+++ src/components/View.js
@@ -3,31 +3,68 @@ import axios from "axios";
 import '../view.css'
 
 function View(props) {
-  const [forms, setForms] = useState([]);
+  const [bugForms, setBugForms] = useState([]);
+  const [featureForms, setFeatureForms] = useState([]);
+
+  const sortBugs = (formList) => {
+    return (formList.filter(item => item.requestType === 'bugReport'))
+  }
+
+  const sortFeatures = (formList) => {
+    return (formList.filter(item => item.requestType === 'featureRequest'))
+  }
+
+  const sortFormsOldest = (formList) => {
+    return (formList.sort((a,b) => {
+    return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    }))
+  }
+
+  const sortFormsNewest = (formList) => {
+    return sortFormsOldest(formList).reverse();
+  }
 
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get((process.env.REACT_APP_API_URL || 'http://localhost:3001') + '/bugs');
-      setForms(response.data);
+      setBugForms(sortBugs(response.data));
+      setFeatureForms(sortFeatures(response.data));
+      // 
     }
     fetchData();
   }, [])
 
   return (
-    <div>
+    <div className="grid-container">
       <div className="topbar">
         <h1>Bug Tracker/Feature Requests</h1>
       </div>
-      {forms.map((item) => {
-        console.log(item)
-        return (
-          <div id={item.id} className="card">
-            {item.username}<br/>
-            {item.email}<br/>
-            {item.message}<br/>
-          </div>
-        )
-      })}
+      <div className="bugCards">
+        <h3>Bug Reports</h3>
+        {bugForms.map((item) => {
+          console.log(item)
+          return (
+            <div key={item.id} className="card">
+              {item.username}<br/>
+              {item.email}<br/>
+              {item.message}<br/>
+            </div>
+          )
+        })}
+      </div>
+      <div className="featureCards">
+        <h3>Feature Requests</h3>
+        {featureForms.map((item) => {
+          console.log(item);
+          return (
+            <div key={item.id} className="card">
+              {item.username}<br/>
+              {item.email}<br/>
+              {item.message}<br/>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }

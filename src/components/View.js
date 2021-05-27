1a52a5ee542fdef5f2f75fde3c0847bd15fc9811
Devin Kenney <devin.kenney111@gmail.com>
2021-05-27 12:54:48 -0400

    Added completed columns and some styling

--- src/components/View.js
+++ src/components/View.js
@@ -3,15 +3,19 @@ import axios from "axios";
 import '../view.css'
 
 function View(props) {
+  const [completed, setCompleted] = useState([]);
+  const [notCompleted, setNotCompleted] = useState([]);
   const [bugForms, setBugForms] = useState([]);
+  const [completedBugs, setCompletedBugs] = useState([]);
   const [featureForms, setFeatureForms] = useState([]);
+  const [completedFeatures, setCompletedFeatures] = useState([]);
 
-  const sortBugs = (formList) => {
-    return (formList.filter(item => item.requestType === 'bugReport'))
+  const sortCompleted = (formList, type) => {
+    return (formList.filter(item => item.completed === type));
   }
 
-  const sortFeatures = (formList) => {
-    return (formList.filter(item => item.requestType === 'featureRequest'))
+  const sortType = (formList, toSort) => {
+    return (formList.filter(item => item.requestType === toSort))
   }
 
   const sortFormsOldest = (formList) => {
@@ -27,9 +31,12 @@ function View(props) {
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get((process.env.REACT_APP_API_URL || 'http://localhost:3001') + '/bugs');
-      setBugForms(sortBugs(response.data));
-      setFeatureForms(sortFeatures(response.data));
-      // 
+      await setCompleted(sortCompleted(response.data, true));
+      await setNotCompleted(sortCompleted(response.data, false))
+      setBugForms(sortType(notCompleted, 'bugReport'));
+      setCompletedBugs(sortType(completed, 'bugReport'));
+      setFeatureForms(sortType(notCompleted, 'featureRequest'));
+      setCompletedFeatures(sortType(completed, 'featureRequest'))
     }
     fetchData();
   }, [])
@@ -42,12 +49,33 @@ function View(props) {
       <div className="bugCards">
         <h3>Bug Reports</h3>
         {bugForms.map((item) => {
-          console.log(item)
           return (
             <div key={item.id} className="card">
-              {item.username}<br/>
-              {item.email}<br/>
-              {item.message}<br/>
+              <div className="infoContainer">
+                {item.username}<br/>
+                {item.email}<br/>
+                {item.message}<br/>
+              </div>
+              <div className="buttonContainer">
+                <button>Complete</button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className="bugCompleted">
+        <h3>Completed Bugs</h3>
+        {completedBugs.map((item) => {
+          return (
+            <div key={item.id} className="card completed">
+              <div className="infoContainer">
+                {item.username}<br/>
+                {item.email}<br/>
+                {item.message}<br/>
+              </div>
+              <div className="buttonContainer">
+                <button>Complete</button>
+              </div>
             </div>
           )
         })}
@@ -55,12 +83,33 @@ function View(props) {
       <div className="featureCards">
         <h3>Feature Requests</h3>
         {featureForms.map((item) => {
-          console.log(item);
           return (
             <div key={item.id} className="card">
-              {item.username}<br/>
-              {item.email}<br/>
-              {item.message}<br/>
+              <div className="infoContainer">
+                {item.username}<br/>
+                {item.email}<br/>
+                {item.message}<br/>
+              </div>
+              <div className="buttonContainer">
+                <button>Complete</button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className="featureCompleted">
+        <h3>Completed Feature Requests</h3>
+        {completedFeatures.map((item) => {
+          return (
+            <div key={item.id} className="card completed">
+              <div className="infoContainer">
+                {item.username}<br/>
+                {item.email}<br/>
+                {item.message}<br/>
+              </div>
+              <div className="buttonContainer">
+                <button>Complete</button>
+              </div>
             </div>
           )
         })}

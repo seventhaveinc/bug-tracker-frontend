63ac4dd1a421606d5c9b11c101acf41a9fad5f00
Devin Kenney <devin.kenney111@gmail.com>
2021-07-09 11:27:22 -0400

    finishing touches

--- src/components/Denied.js
+++ src/components/Denied.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect, } from "react";
 import axios from "axios";
 import { useHistory } from 'react-router-dom'
+import '../view.css'
 
 function Denied (props) {
 
+  let history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/login')
+    }, 5000);
+  }, [])
+
   return (
-    <div>
-      Access Denied
+    <div className="denied">
+      <h1>Access Denied</h1>
+      <h3>Redirecting in 5 seconds.</h3>
     </div>
   )
 }
